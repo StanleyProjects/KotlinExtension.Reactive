e14577b3e636c84455487e6bf11eee773dd9a6b2
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-28 21:24:57 +0300

    unstable...

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
     apply(P.kotlinJvm)
 }
 
-project.version = Version.name
-
 dependencies {
     testImplementation(D.jupiterApi)
     testRuntimeOnly(D.jupiterEngine)
@@ -13,4 +11,38 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+project.version = Version.name
+
+val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
+
+"Snapshot".also { type ->
+    val groupId = "com.github.kepocnhh"
+    val artifactId = rootProject.name
+    val versionName = project.version.toString() + "-SNAPSHOT"
+    task<Jar>("assemble$type") {
+        dependsOn(compileKotlin)
+        archiveBaseName.set(artifactId)
+        archiveVersion.set(versionName)
+        from(compileKotlin.destinationDir)
+    }
+    task("assemble${type}Pom") {
+        doLast {
+            val parent = File(buildDir, "libs")
+            if (!parent.exists()) parent.mkdirs()
+            val file = File(parent, "$artifactId-$versionName.pom")
+            if (file.exists()) file.delete()
+            file.createNewFile()
+            check(file.exists()) { "File by path: ${file.absolutePath} must be exists!" }
+            val text = MavenUtil.pom(
+                modelVersion = "4.0.0",
+                groupId = groupId,
+                artifactId = artifactId,
+                version = versionName,
+                packaging = "jar"
+            )
+            file.writeText(text)
+        }
+    }
 }
